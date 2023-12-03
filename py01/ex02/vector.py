fd42e303c0cbeb462370fd2a507446c2cc33576b
Mafranco31 <mat.francois31@gmail.com>
2023-12-03 19:41:37 +0100

    try

--- py01/ex02/vector.py
+++ py01/ex02/vector.py
@@ -81,3 +81,73 @@ class Vector:
             return "Vector({})".format(self.values[0])
         else: 
             return "Vector({})".format(self.values)
+    def __add__(self, v2):
+        if isinstance(v2, Vector):
+            if self.shape == v2.shape:
+                if self.shape == (self.size, 1):
+                    ret = []
+                    i = 0
+                    while i < self.size:
+                        ret.append([self.values[i][0] + v2.values[i][0]])
+                        i += 1
+                    return Vector(ret)
+                else:
+                    ret = [[]]
+                    i = 0
+                    while i < self.size:
+                        ret[0].append(self.values[0][i] + v2.values[0][i])
+                        i += 1
+                    return Vector(ret)
+            else:
+                raise ValueError("to add two vectors, they must have the same shape")
+        else:
+            raise ValueError("Addition is only defined between two vectors")
+    def __radd__(self, v2):
+        return self.__add__(v2)
+    def __sub__(self, v2):
+        if isinstance(v2, Vector):
+            if self.shape == v2.shape:
+                if self.shape == (self.size, 1):
+                    ret = []
+                    i = 0
+                    while i < self.size:
+                        ret.append([self.values[i][0] - v2.values[i][0]])
+                        i += 1
+                    return Vector(ret)
+                else:
+                    ret = [[]]
+                    i = 0
+                    while i < self.size:
+                        ret[0].append(self.values[0][i] - v2.values[0][i])
+                        i += 1
+                    return Vector(ret)
+            else:
+                raise ValueError("to substract two vectors, they must have the same shape")
+        else:
+            raise ValueError("Substraction is only defined between two vectors")
+    def __rsub__(self, v2):
+        return self.__sub__(v2)
+    def __truediv__(self, num):
+        if isinstance(num, float) or isinstance(num, int):
+            if num != 0:
+                if self.shape == (self.size, 1):
+                    ret = []
+                    i = 0
+                    while i < self.size:
+                        ret.append([self.values[i][0] / num])
+                        i += 1
+                    return Vector(ret)
+                else:
+                    ret = [[]]
+                    i = 0
+                    while i < self.size:
+                        ret[0].append(self.values[0][i] / num)
+                        i += 1
+                    return Vector(ret)
+            else:
+                raise ValueError("Division by zero")
+        else:
+            raise ValueError("Division is only defined between a vector and a number")
+    def __rtruediv__(self, v2):
+        return self.__truediv__(v2)
+    
